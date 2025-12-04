b15504533da7fbfc1251dae597df0bb24a381956
ebazo222 <edna.bazo@hotmail>
2025-12-04 17:39:01 -0300

    PUT e DELETE

--- test/API/pet.spec.js
+++ test/API/pet.spec.js
@@ -24,4 +24,45 @@ describe('API Petstore Swagger - Entidade Pet', () =>{
             })
     }) // Final do POST
     // Primeiro teste que faz a inclusão do Cao(Kira)
-} )
+    it('Get Pet',() => {
+        return request
+           // .get('/pet/ + petId')// Tradicional
+           .get(`/pet/${petId}`)//actual
+            .then((response) => {
+                console.log('Resposta Recebida:\n', response.body)
+                expect(response.statusCode).toBe(200)
+                expect(response.body.id).toBe(petId)
+                expect(response.body.name).toBe('Kira')
+                expect(response.body.category.name).toBe('dog')
+                expect(response.body.tags[0].name).toBe('vacinado')
+                expect(response.body.status).toBe('available')
+            })
+
+        }) // Final do GET
+
+        it('PUT Pet', () => {
+
+            const pet = require('../../Vendors/json/petput.json')
+
+            return request
+            .put('/pet')
+            .send(pet)
+            .then((response) =>{
+            expect(response.statusCode).toEqual(200)
+            expect(response.body.status).toEqual('sold')
+            })
+        }) // Final do PUT
+
+        it('DELETE Pet', () =>{
+            return request
+            .delete(`/pet/${petId}`)
+            .then((response) => {
+                expect(response.statusCode).toEqual(200)
+                expect(response.body.code).toEqual(200)
+                expect(response.body.type).toBe('unknown')
+                expect(response.body.message).toBe(petId.toString())
+         } )
+                
+            })// Final do Delete
+
+        })

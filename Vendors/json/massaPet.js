a5f08f3a7a97410af8608141a9cf23a94b313e20
ebazo222 <edna.bazo@hotmail>
2025-12-05 11:57:56 -0300

    Post DDT

---
+++ Vendors/json/massaPet.js
@@ -0,0 +1,36 @@
+module.exports.array = [
+  {
+    "id": 5001,
+    "categoryid": 5,
+      "categoryname": "dog",
+        "petname": "Kira",
+      "tag0id": 99,
+        "tag0name": "vacinado",
+        "tag1id": 100,
+        "categoryname": "beagle",
+        "petstatus": "available"
+  },
+  {
+"id": 5002,
+   "categoryid": 6,
+   "categoryname": "cat",
+   "petname": "Marinete",
+   "tag0id": 99,
+   "tag0name": "vacinado",
+   "tag1id": 120,
+   "tag1name": "persian",
+   "petstatus": "available"
+ },
+ {
+   "id": 5003,
+   "categoryid": 7,
+   "categoryname": "fish",
+   "petname": "Nemo",
+   "tag0id": 88,
+   "tag0name": "vermifugado",
+   "tag1id": 130,
+   "tag1name": "clownfish",
+   "petstatus": "available"
+ }
+]
+
--- test/API/pet.spec.js
+++ test/API/pet.spec.js
@@ -6,6 +6,7 @@ const petId = 5001
 describe('API Petstore Swagger - Entidade Pet', () =>{
     // Atributos, objectos, constantes e variáveis
     const request = supertest ('https://petstore.swagger.io/v2') // Base URL
+    const massaPet = require("../../Vendors/json/massaPet")  // Teste de API com uma massa
 
     // Funçãoes e métodos = it
     // Incluir animal
@@ -65,4 +66,60 @@ describe('API Petstore Swagger - Entidade Pet', () =>{
                 
             })// Final do Delete
 
+            // POST Pet DDT- Utilizando massa de teste
+            it.each(massaPet.array.map(elemento => [
+                elemento.id,
+                elemento.categoryid,
+                elemento.categoryname,
+                elemento.petname,
+                elemento.tag0id,
+                elemento.tag0name,
+                elemento.tag1id,
+                elemento.tags1name,
+                elemento.petstatus
+    
+            ])) // lê um bloco de cada vez
+            
+            ('POST Pet Data Driven: %s', (
+                id,
+                categoryid,
+                categoryname,
+                petname,
+                tag0id,
+                tag0name,
+                tag1id,
+                tag1name,
+                petstatus
+
+            // informaçao que vai diferenciar uma da outra, estavam disponiveis para o it, agora, para serem usados
+        ) => {
+            // Modelo de json a ser enviado
+            // Valores que não forem sobrepostos seguirão no modelo
+                const pet = require("../../Vendors/json/pet.json")
+
+                // Sobreposição de valores
+                pet.id = id
+                pet.category.id = categoryid
+                pet.category.name = categoryname
+                pet.name = petname
+                pet.tags[0].id = tag0id
+                pet.tags[0].name = tag0name
+                pet.tags[1].id = tag1id
+                pet.tags[1].name = tag1name
+                pet.status = petstatus
+                // esses são parametros individuais, dai a falta da virgula, caso contrário significa que estão na mesma linha
+       //Teste em si 
+       return request
+       .post('/pet')
+       .send(pet)
+       .then((response) => {
+            expect(response.statusCode).toBe(200)
+            expect(response.body.id).toBe(id)
+            expect(response.body.name).toBe(petname)
+            expect(response.body.category.name).toBe(categoryname)
+            expect(response.body.tags[0].name).toBe(tag0name)
+                
+
+       })
+            }) //final do POST Pet DDT
         })
